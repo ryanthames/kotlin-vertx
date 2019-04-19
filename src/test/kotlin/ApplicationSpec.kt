23e750fd3533de31404e6a3f0252edc88c44c18c
Ryan Thames <ryanthames@gmail.com>
2019-04-19 07:34:30 -0500

    Testing chapter

---
+++ src/test/kotlin/ApplicationSpec.kt
@@ -0,0 +1,54 @@
+import com.github.kittinunf.fuel.core.Client
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import io.kotlintest.eventually
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import services.SunService
+import java.net.URL
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+class FuelTestClient(val testResponse: Response): Client {
+  override fun executeRequest(request: Request): Response {
+    return testResponse
+  }
+}
+
+class ApplicationSpec : StringSpec() {
+  init {
+    "DateTimeFormat must return 1970 as the beginning of the epoc" {
+      val beginning = ZonedDateTime.ofInstant(Instant.ofEpochSecond(0), ZoneId.systemDefault())
+      val formattedYear = beginning.format(DateTimeFormatter.ofPattern("YYYY"))
+      formattedYear shouldBe "1970"
+    }
+
+    "SunService must retrieve correct sunset and sunrise information" {
+      val json = """{
+        "results": {
+        "sunrise": "2016-04-14T20:18:12+00:00",
+        "sunset": "2016-04-15T07:31:52+00:00",
+        }
+       }
+      """.trimMargin()
+      val testResponse = Response(URL("http://localhost/api/data"))
+      testResponse.data = json.toByteArray()
+      val testClient = FuelTestClient(testResponse)
+      FuelManager.instance.client = testClient
+
+      val lat = -33.8830
+      val lon = 151.2167
+      val sunService = SunService()
+      val resultP = sunService.getSunInfo(lat, lon)
+
+      eventually(Duration.ofSeconds(5)) {
+        val sunInfo = resultP.get()
+        sunInfo.sunrise shouldBe "06:18:12"
+      }
+    }
+  }
+}
